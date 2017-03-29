e3c05e8c5c7983b945e57182f19b5f636b0a8c3a
Matheus Barba <matheus.barba.lemaf@gmail.com>
2017-03-29 14:27:05 -0300

    Adiciona logger

--- deploy.js
+++ deploy.js
@@ -1,16 +1,53 @@
 global.config = require('konfig')();
 var ncp = require('ncp').ncp;
+var git = require('git-rev');
+var gitconfig = require('git-config');
+var fs = require('fs');
+var moment = require('moment');
 
 function deploy() {
-	var source = config.deploy;
+	var source = config.deploy.pastas;
 	for (var item in source) {
 		upload(item);
 	}
+	gerarLog();
 }
 
-function upload(item) {
-	ncp(config.deploy[item].origem, config.deploy[item].destino, (err) => {
- 		console.log(err);
+function upload(item) {0
+	ncp(config.deploy.pastas[item].origem, config.deploy.pastas[item].destino, (err) => {
+		if (!err) {
+			console.log(item + ' foi publicado com sucesso!');
+		} else {
+ 			console.log('Erro ao publicar ' + item);
+		}
+	});
+}
+
+function gerarLog() {
+	gitconfig((err, config) => {
+		git.short((commit) => {
+			config.commit = commit;
+
+			git.branch((branch) => {
+				config.branch = branch;
+				log(config);
+			});
+		});
+	});
+	
+}
+
+function log(gitConfig) {
+	var data = moment().format('DD/MM/YYYY HH:mm');
+	var user = gitConfig.user.name;
+	var commit = gitConfig.commit;
+	var branch = gitConfig.branch;
+	var entry = "[" + data + "] " + user + " publicou o commit #"+ commit +" da branch " + branch + " \r\n";
+	console.log(git);
+	fs.appendFile(config.deploy.log, entry, function(err) {
+		if (err) {
+			console.log('Erro ao gerar log!', err);
+		}
 	});
 }
 
